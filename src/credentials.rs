4dd466aff574db86ccdbb15afb020eb7667059d3
Joscha Loos <j.loos@posteo.net>
2025-05-19 22:06:05 +0200

    feat: implement serde for

--- src/credentials.rs
+++ src/credentials.rs
@@ -1,3 +1,8 @@
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum Credentials {
     Basic(String, String),
     Bearer(String),
